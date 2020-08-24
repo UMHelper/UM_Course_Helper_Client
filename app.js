2760fc5ef864ebb80d9a42afc0b85a08b96b4d77
boxmars <2803726925@qq.com>
2020-08-25 00:14:08 +0800

    Search Page

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 App({
+  globalData:{
 
+  },
+  data:{
+
+  },
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
--- pages/search/search.js
+++ pages/search/search.js
@@ -1,6 +1,5 @@
 // pages/search/search.js
 Page({
-
   /**
    * 页面的初始数据
    */
